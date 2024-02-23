bfdace28943d9cafe65b12385f3a154586bb4b04
adithyaac <133075878+adithyaac@users.noreply.github.com>
2024-02-23 23:22:59 +0530

    Updated app1_mysql.py

--- app1_mysql.py
+++ app1_mysql.py
@@ -23,23 +23,7 @@ def populate_audio_library():
     sql = "INSERT INTO audio_library (id,audio_name, audio_artist, audio_genre, audio_path) VALUES (%s, %s, %s, %s, %s)"
     cursor.executemany(sql, audio_files)
     mydb.commit()
-# def search_images(keyword):
-#     sql = "SELECT * FROM uploaded_images WHERE image_name LIKE %s"
-#     cursor.execute(sql, ('%' + keyword + '%',))
-#     return cursor.fetchall()
-
-# def search_audio(keyword):
-#     sql = "SELECT * FROM audio_library WHERE audio_name LIKE %s OR audio_genre LIKE %s"
-#     cursor.execute(sql, ('%' + keyword + '%', '%' + keyword + '%'))
-#     return cursor.fetchall()
-
-# @app.route('/search', methods=['GET'])
-# def search_media():
-#     keyword = request.args.get('keyword')
-#     search_results = search_images(keyword)
-#     search_results = search_audio(keyword)
-#     return jsonify(search_results)
-
+    
 populate_audio_library()
 if __name__ == '__main__':
     app.run(debug=True)
