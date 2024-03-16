e12925ef58e6c86680ac3405caeb846448bbf140
adithyaac <133075878+adithyaac@users.noreply.github.com>
2024-03-16 14:36:22 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,23 +1,25 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL
+create table users (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   username VARCHAR(50) NOT NULL,
+   email VARCHAR(100) NOT NULL,
+   password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE uploaded_images (
-
-    image_name VARCHAR(100) NOT NULL,
-    id INT,
-    FOREIGN KEY (id) REFERENCES users(id)
+create table uploaded_images (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   user_id INT,
+   FOREIGN KEY (user_id) REFERENCES users(id),
+   image_name VARCHAR(100) NOT NULL,
+   fsize FLOAT,
+   bindata LONGBLOB,
+   upload_date TIMESTAMP
 );
 
-CREATE TABLE uploaded_images (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    image_name VARCHAR(100) NOT NULL,
-    image_description TEXT,
-    image_path VARCHAR(255) NOT NULL,
-    upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+create table audio_library (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   audio_name VARCHAR(100) NOT NULL,
+   duration INT,
+   fszie BIGINT,
+   bindata LONGBLOB,
+   upload_date TIMESTAMP
 );
